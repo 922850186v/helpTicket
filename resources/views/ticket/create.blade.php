e24ab6adcc9e8ce27687abc783fa3b24e6980a2b
Vishva Isuranga <96844796+922850186v@users.noreply.github.com>
2024-05-27 13:26:19 +0530

    27052024_InitialChanges

---
+++ resources/views/ticket/create.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <div style="color:white">you are ticket/create</div>
+</x-app-layout>
---
+++ resources/views/ticket/store.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <div style="color:white">This is ticket/store</div>
+</x-app-layout>
